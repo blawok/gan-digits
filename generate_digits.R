745847e672f147f1944556e3e57e41944c4fec1b
Bartek <kowalczuk.bartek@gmail.com>
2019-05-25 12:04:22 +0200

    restructure repo

--- generate_digits.R
+++ generate_digits.R
@@ -1,3 +1,4 @@
+
 generate_digits <- function(digit_to_generate = NULL, show_data = FALSE, save_data = FALSE){
   
   if (is.numeric(digit_to_generate)) {
@@ -5,11 +6,12 @@ generate_digits <- function(digit_to_generate = NULL, show_data = FALSE, save_da
     source("gan.R")
   }
   
+  
   if (!is.numeric(digit_to_generate)) {
     print("Specify desired digit as an integer")
   } else if (digit_to_generate == 3) {
       # load desired model data
-      load("model_3.RData")
+      load("neural_network/model_3.RData")
       
       # declare model hyperparameters
       g_nn<<-nn_model(input_dim=784,hidden=10,output_dim=784,learningrate=0.1,
@@ -21,9 +23,11 @@ generate_digits <- function(digit_to_generate = NULL, show_data = FALSE, save_da
       generation<-generator(gan_model,6)
     }
   
+  
   if (!is.logical(show_data) | !is.logical(save_data)) {
     print("'show_data' and 'save_data' have to be logical") 
   }
+  
   
   if (isTRUE(show_data) & is.numeric(digit_to_generate)) {
     # draw new data (remember to enlarge plot window!)
@@ -36,6 +40,7 @@ generate_digits <- function(digit_to_generate = NULL, show_data = FALSE, save_da
     )
   }
 
+  
   if (isTRUE(save_data) & is.numeric(digit_to_generate)){
     # save data to a file
     write.csv(generation, "generation_test.csv")
---
+++ neural_network/data_prepare.R
@@ -0,0 +1,46 @@
+library(imager)
+
+data_prepare <- function(folder_path="3/", destination_file = "threes_full.csv", image_quantity = 10, resizing_to = 28) {
+  
+  # loading all file names into one list
+  files <- list.files(path=folder_path, pattern="*.PNG", full.names=TRUE, recursive=FALSE)
+  
+  df <- data.frame()
+  image_number = 0
+  
+  # resizing images and transforming them into 1-dim vectors and appending them to one dataframe
+  for (file in files) {
+    if (image_number <= image_quantity) {
+      dfTemp <-  file %>%
+        load.image() %>%
+        resize(., size_x=resizing_to, size_y=resizing_to) %>%
+        as.matrix(.) %>%
+        as.vector(.) %>%
+        matrix(., nrow = 1) %>%
+        data.frame(.)
+
+      df <- rbind(df, dfTemp)
+    }
+    image_number = image_number + 1
+  }
+  
+  # encoding values to either 0 or 255 for better distinction
+  color_update <- function(x){
+    if (x == 1) {
+      x <- 0
+    } else if (x == 0) {
+      x <- 255
+    }
+  }
+  
+  df <- data.frame(apply(df, c(1,2), color_update))
+  
+  # saving file
+  write.csv(df, file = destination_file)
+}
+
+# # example of usage
+# data_prepare(folder_path="3/",
+#              destination_file = "threes_full.csv",
+#              image_quantity = 10,
+#              resizing_to = 28)
